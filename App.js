8944f264d94b6a37dd0d18d32068844210705852
6210110689 <6210110689@psu.ac.th>
2021-07-22 03:11:08 +0700

    5.1

--- App.js
+++ App.js
@@ -2,22 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Weather from './components/Weather';
-import constants from 'expo-constants';
+
 
 export default function App() {
  
  return (
- <View style={styles.container}>
-  <Weather zipCode="90110"/>
-  <StatusBar style="auto" />
- </View>
+
  );
 }
 
-const styles = StyleSheet.create({
- container: {
-    paddingTop: constants.statusBarHeight
- },
-
-}
-);
