46f4f6395a09d7229e385408fb028f710abe31a0
6210110689 <6210110689@psu.ac.th>
2021-07-22 00:29:12 +0700

    Check 2

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { ImageBackground, StyleSheet, Text } from "react-native"
 import Forecast from "./Forecast"
 
 export default function Weather(props) {
@@ -8,7 +9,19 @@ export default function Weather(props) {
         temp: 0
     })
     return (
-        <Forecast {...forecastInfo}/>
+        <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+            <Text>Zip Code</Text>
+            <Text>{props.zipCode}</Text>
+            <Forecast {...forecastInfo}/>
+        </ImageBackground>
+        
     )
    }
    
+   const styles = StyleSheet.create({
+       backdrop: {
+           alignItems: 'center',
+           width: '100%',
+           height: '100%'
+       }
+   })
